8bde50723ecbfd309d4837efc074cede3c989308
Yifu <ybaumes@gmail.com>
2013-05-03 18:34:57 +0200

    Reorder ede init with functions definition it depends on.

--- init.el
+++ init.el
@@ -1,42 +1,3 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; YBA ven. 03 mai 2013 10:41:17 CEST
-
-;; Load CEDET.
-;; See cedet/common/cedet.info for configuration details.
-;; IMPORTANT: Tou must place this *before* any CEDET component (including
-;; EIEIO) gets activated by another package (Gnus, auth-source, ...).
-(load-file "~/bzr/cedet/cedet-devel-load.el")
-
-;; Add further minor-modes to be enabled by semantic-mode.
-;; See doc-string of `semantic-default-submodes' for other things
-;; you can use here.
-(add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode t)
-(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
-(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
-(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
-
-;; Enable Semantic
-(semantic-mode t)
-
-;; Enable EDE (Project Management) features
-(global-ede-mode t)
-
-(require 'semantic/ia)
-;(require 'semantic/bovine/gcc)
-(require 'semantic/bovine/clang)
-(semantic-clang-activate)
-(semantic-add-system-include "/usr/local/include/boost/" 'c++-mode)
-
-(ede-cpp-root-project "pdk-software"
-                      :name "pdk software"
-                      :file "~/git/pdk-software/CMakeLists.txt"
-                      :include-path (get-header-directories "~/git/pdk-software")
-                      :system-include-path nil
-                      :spp-table '(("LINUX" . "")
-                                   ("_REENTRANT" . "")
-                                   ("MULTISESSION_TOE" . "")
-                                   ("_FORTIFY_SOURCE" . "")
-                                   ("FORCE_INLINE" . "__attribute__((always_inline)) inline")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; yba ven. 03 mai 2013 11:58:40 CEST
@@ -124,6 +85,43 @@
    header-directories))
 
 ;;(get-header-directories "~/git/pdk-software")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; YBA ven. 03 mai 2013 10:41:17 CEST
+
+;; Load CEDET.
+;; See cedet/common/cedet.info for configuration details.
+;; IMPORTANT: Tou must place this *before* any CEDET component (including
+;; EIEIO) gets activated by another package (Gnus, auth-source, ...).
+(load-file "~/bzr/cedet/cedet-devel-load.el")
+
+;; Add further minor-modes to be enabled by semantic-mode.
+;; See doc-string of `semantic-default-submodes' for other things
+;; you can use here.
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode t)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
+(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
+
+(semantic-mode t)
+(global-ede-mode t)
+
+(require 'semantic/ia)
+;(require 'semantic/bovine/gcc)
+(require 'semantic/bovine/clang)
+(semantic-clang-activate)
+(semantic-add-system-include "/usr/local/include/boost/" 'c++-mode)
+
+(ede-cpp-root-project "pdk-software"
+                      :name "pdk software"
+                      :file "~/git/pdk-software/CMakeLists.txt"
+                      :include-path (get-header-directories "~/git/pdk-software")
+                      :system-include-path nil
+                      :spp-table '(("LINUX" . "")
+                                   ("_REENTRANT" . "")
+                                   ("MULTISESSION_TOE" . "")
+                                   ("_FORTIFY_SOURCE" . "")
+                                   ("FORCE_INLINE" . "__attribute__((always_inline)) inline")))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq visible-bell t
       inhibit-startup-screen t)
